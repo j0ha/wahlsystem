6168be778a85b296b3ba4cd21dace2eb291e5a68
Johannes Schur <contact@johannesschur.com>
2020-08-16 22:54:37 +0200

    Created terminal route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//TESTROUTE
+Route::get('/testRoute', 'testController@querryTerminal');
+
+//ROUTE FOR TERMINAL
+
+Route::get('/election/vote/{electionUUID}/{terminalUUID}', 'terminalController@verifyTruthiness', ['electionUUID' => 'electionUUID'], ['terminalUUID' => 'terminalUUID']);

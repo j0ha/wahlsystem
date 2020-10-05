89ec5569b299f42f1ed44558339d4dfae938252c
Johannes Schur <contact@johannesschur.com>
2020-10-05 21:22:41 +0200

    added laravel login ui

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('surname');
+            $table->string('surname')->default('default');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('approved');
+            $table->boolean('approved')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });

b491231d2a58dca94b0dd08a8229a16b6da46786
Johannes Schur <contact@johannesschur.com>
2020-10-12 13:29:24 +0200

    updated email migration

--- database/migrations/2020_08_10_185638_create_voters_table.php
+++ database/migrations/2020_08_10_185638_create_voters_table.php
@@ -15,19 +15,19 @@ class CreateVotersTable extends Migration
     {
         Schema::create('voters', function (Blueprint $table) {
             $table->id();
-            $table->string('surname');
-            $table->string('name');
-            $table->date('birth_year')->default('1111-11-11');
+            $table->string('surname')->nullable();
+            $table->string('name')->nullable();
+            $table->date('birth_year')->default('1111-11-11')->nullable();
             $table->boolean('voted_via_email')->default(false);
             $table->boolean('voted_via_terminal')->default(false);
             $table->boolean('got_email')->default(false);
             $table->uuid('uuid');
             $table->uuid('direct_uuid');
             $table->char('direct_token')->nullable();
-            $table->string('email');
+            $table->string('email')->nullable();
             $table->integer('election_id');
-            $table->integer('schoolclass_id');
-            $table->integer('form_id');
+            $table->integer('schoolclass_id')->nullable();
+            $table->integer('form_id')->nullable();
             $table->timestamps();
         });
     }

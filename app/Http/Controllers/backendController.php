b952405c22505599f6b163b900b0347744e55913
lennardchristoph <70326471+lennardchristoph@users.noreply.github.com>
2020-10-21 22:16:11 +0200

    e

--- app/Http/Controllers/backendController.php
+++ app/Http/Controllers/backendController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Election;
+use Illuminate\Database\Eloquent\Model;
 
 class backendController extends Controller
 {
@@ -13,6 +14,8 @@ class backendController extends Controller
     }
     public function stats($electionUUID){
       $elections = Election::all();
+
+      $selectedElection = Election::where('UUID', $electionUUID);
 
       return view('backendviews.Stats', ['electionUUID' => $electionUUID])->withElections($elections);
     }

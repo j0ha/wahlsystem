acd931665fdcf3bbd74557614198783dccbe193b
j0ha <46874251+j0ha@users.noreply.github.com>
2020-11-14 10:57:31 +0100

    dashboard home design update

--- resources/views/backendviews/v2/dashboard.blade.php
+++ resources/views/backendviews/v2/dashboard.blade.php
@@ -27,9 +27,9 @@
   <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
       <div class="card border-3 border-top border-top-primary">
           <div class="card-body">
-              <h5 class="text-muted">asd</h5>
+              <h5 class="text-muted">Gezählte Stimmen</h5>
               <div class="metric-value d-inline-block">
-                  <h1 class="mb-1">21322</h1>
+                  <h1 class="mb-1">213442</h1>
               </div>
           </div>
       </div>
@@ -37,9 +37,9 @@
   <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
       <div class="card border-3 border-top border-top-primary">
           <div class="card-body">
-              <h5 class="text-muted">Da</h5>
+              <h5 class="text-muted">Wahlbeteiligung</h5>
               <div class="metric-value d-inline-block">
-                  <h1 class="mb-1">21322</h1>
+                  <h1 class="mb-1">23%</h1>
               </div>
           </div>
       </div>
@@ -105,6 +105,24 @@
 <div class="row">
   <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
       <div class="card">
+          <h5 class="card-header">Nutzungsverteilung der Terminals</h5>
+          <div class="card-body">
+              <div id="terminal_usage_dounut"></div>
+          </div>
+      </div>
+  </div>
+  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+      <div class="card">
+          <h5 class="card-header">Wahlbeteiligung Klassen</h5>
+          <div class="card-body">
+              <div id="klassenwahlbeteiligung_bar"></div>
+          </div>
+      </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+      <div class="card">
           <h5 class="card-header">Nutzerverteilung</h5>
           <div class="card-body">
               <div id="jahrgang_dounut"></div>
@@ -116,7 +134,7 @@
       <div class="card">
           <h5 class="card-header">Klassenverteilung</h5>
           <div class="card-body">
-              <div id="klassen_bar"></div>
+              <div id="klassenverteilung_bar"></div>
           </div>
       </div>
   </div>
@@ -153,8 +171,74 @@
           resize: true
     });
 
+    new Morris.Donut({
+        element: 'terminal_usage_dounut',
+        data: [
+            { value: 40, label: 'Peter 1' },
+            { value: 20, label: 'Peter 2' },
+            { value: 40, label: 'Peter 3' }
+        ],
+
+        labelColor: '#2e2f39',
+           gridTextSize: '14px',
+        colors: [
+             "#5969ff",
+                        "#ff407b",
+                        "#25d5f2",
+                        "#ffc750"
+
+        ],
+
+        formatter: function(x) { return x + "%" },
+          resize: true
+    });
+
     new Morris.Bar({
-        element: 'klassen_bar',
+        element: 'klassenverteilung_bar',
+        data: [
+            { x: '11b', y: 10 },
+            { x: '11b', y: 10 },
+            { x: '11b', y: 11 },
+            { x: '11b', y: 2 },
+            { x: '11b', y: 32 },
+            { x: '11b', y: 14 },
+            { x: '11b', y: 15 },
+            { x: '11b', y: 26 },
+            { x: '11b', y: 37 },
+            { x: '11b', y: 10 },
+            { x: '11b', y: 11 },
+            { x: '11b', y: 2 },
+            { x: '11b', y: 32 },
+            { x: '11b', y: 14 },
+            { x: '11b', y: 15 },
+            { x: '11b', y: 26 },
+            { x: '11b', y: 37 },
+            { x: '11b', y: 11 },
+            { x: '11b', y: 37 },
+            { x: '11b', y: 11 },
+            { x: '11b', y: 2 },
+            { x: '11b', y: 32 },
+            { x: '11b', y: 14 },
+            { x: '11b', y: 15 },
+            { x: '11b', y: 2 },
+            { x: '11b', y: 32 },
+            { x: '11b', y: 14 },
+            { x: '11b', y: 15 },
+            { x: '11b', y: 26 },
+            { x: '11b', y: 37 },
+            { x: '11b', y: 8 }
+        ],
+        xkey: 'x',
+        ykeys: ['y'],
+        labels: ['Y'],
+           barColors: ['#5969ff'],
+             resize: true,
+                gridTextSize: '14px'
+
+    });
+
+    new Morris.Bar({
+        element: 'klassenwahlbeteiligung_bar',
         data: [
             { x: '11b', y: 10 },
             { x: '11b', y: 10 },

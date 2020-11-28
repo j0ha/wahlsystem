e573ea8b143b95ef2136814c5bac1c690636ee5d
j0ha <46874251+j0ha@users.noreply.github.com>
2020-11-28 20:41:06 +0100

    added voters birth view

---
+++ resources/views/vote/spv/birthVerification.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title></title>
+    <style media="screen">
+          html,body {
+            height: 100%;
+          }
+    </style>
+  </head>
+  <body style="height:100vh; width:100vw;">
+    <div class="container-fluid h-100 w-100 d-inline-block px-0 mx-0">
+      <div class="row h-90 w-100 px-0 mx-0">
+        <div class="col d-flex">
+
+          <div class="row w-100 h-5 align-self-center">
+            <div class="mx-auto">
+              <p>Bitte gebe deinen Geburtstag ein um dich zu verifizieren.</p>
+              <div class="mx-auto">
+                <input type="number" name="" value="" placeholder="16" class="form-less">.<input type="number" name="" value=""placeholder="12" class="form-less">.<input type="number" name="" value=""placeholder="2000" class="form-less">
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div class="row h-10 w-100 px-0 mx-0">
+        <button type="button" name="button" class="btn btn-primary btn-lg btn-block px-0 mx-0">Zur Abstimmung</button>
+      </div>
+    </div>
+  </body>
+</html>

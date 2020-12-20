07dfd2a3c49616327ce939b0e0bffff5c7b46915
j0ha <46874251+j0ha@users.noreply.github.com>
2020-12-20 12:05:58 +0100

    updated new vote styles

--- resources/views/vote/spv/birthVerification.blade.php
+++ resources/views/vote/spv/birthVerification.blade.php
@@ -18,25 +18,34 @@
     </style>
   </head>
   <body style="height:100vh; width:100vw;">
-    <div class="container-fluid h-100 w-100 d-inline-block px-0 mx-0">
-      <div class="row h-90 w-100 px-0 mx-0">
-        <div class="col d-flex">
+    <div class="d-flex eao-vote-header">
+      <span class="display-4">Bitte verifiziere Dich!</span>
+    </div>
+    <div class="eao-vote-content">
+      <div class="container-fluid h-100 w-100 d-inline-block px-0 mx-0">
+        <div class="row h-90 w-100 px-0 mx-0">
+          <div class="col d-flex">
 
-          <div class="row w-100 h-5 align-self-center">
-            <div class="mx-auto">
-              <p>Bitte gebe deinen Geburtstag ein um dich zu verifizieren.</p>
+            <div class="row w-100 h-5 align-self-center">
               <div class="mx-auto">
-                <input type="number" name="" value="" placeholder="16" class="form-less">.<input type="number" name="" value=""placeholder="12" class="form-less">.<input type="number" name="" value=""placeholder="2000" class="form-less">
-              </div>
+                <p>Bitte gebe deinen Geburtstag ein um dich zu verifizieren.</p>
+                <div class="mx-auto">
+                  <input type="number" name="" value="" placeholder="16" class="form-less">.<input type="number" name="" value=""placeholder="12" class="form-less">.<input type="number" name="" value=""placeholder="2000" class="form-less">
+                </div>
 
+              </div>
             </div>
           </div>
-        </div>
 
+        </div>
+        <div class="row h-10 w-100 px-0 mx-0">
+          <button type="button" name="button" class="btn btn-primary btn-lg btn-block px-0 mx-0">Zur Abstimmung</button>
+        </div>
       </div>
-      <div class="row h-10 w-100 px-0 mx-0">
-        <button type="button" name="button" class="btn btn-primary btn-lg btn-block px-0 mx-0">Zur Abstimmung</button>
-      </div>
+    </div>
+
+    <div class="d-flex eao-vote-footer">
+      <span>Schulsprecherwahl STS Walddórfer</span>
     </div>
   </body>
 </html>
--- resources/views/vote/spv/candidates.blade.php
+++ resources/views/vote/spv/candidates.blade.php
@@ -17,29 +17,46 @@
           }
     </style>
   </head>
-  <body style="height:100vh; width:100vw;">
-    <div class="container-fluid h-100 w-100 d-inline-block px-0 mx-0">
-      <div class="row h-90 w-100 px-0 mx-0">
-        <div class="col h-100 px-0 bg-secondary img-cover justify-content-center" style="background-image: url(https://images.unsplash.com/photo-1606274741559-d322810275c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80);">
-            <div class="align-self-center w-75">
-              <h1 class="display-1">
-                Hans Pater Team
-              </h1>
-              <p>Dies hier ist eine kurze beschreibung von dem Team und seinen fähigkeiten. Was sie alles so machen und tun und machen unfd tun und machen und tun und machen. Dies hier ist eine kurze beschreibung von dem Team</p>
-            </div>
+  <body class="eao-vote-body">
+    <div class="d-flex eao-vote-header">
+      <span class="display-4">Bitte stimme ab!</span>
+    </div>
+    <div class="h-90 w-100">
+      <div class="container-fluid h-100 w-100 d-inline-block px-0 mx-0">
+        <div class="row h-90 w-100 px-0 mx-0">
+
+          <div class="col h-100 px-0 bg-secondary img-cover justify-content-center" style="background-image: url(https://images.unsplash.com/photo-1606274741559-d322810275c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80);">
+              <div class="align-self-center w-75">
+                <h1 class="display-1">
+                  Hans Pater Team
+                </h1>
+                <p>Dies hier ist eine kurze beschreibung von dem Team und seinen fähigkeiten. Was sie alles so machen und tun und machen unfd tun und machen und tun und machen. Dies hier ist eine kurze beschreibung von dem Team</p>
+              </div>
+          </div>
+          <div class="col h-100 px-0 bg-secondary img-cover justify-content-center" style="background-image: url(https://images.unsplash.com/photo-1606274741559-d322810275c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80);">
+              <div class="align-self-center w-75">
+                <h1 class="display-1">
+                  Hans Pater Team
+                </h1>
+                <p>Dies hier ist eine kurze beschreibung von dem Team und seinen fähigkeiten. Was sie alles so machen und tun und machen unfd tun und machen und tun und machen. Dies hier ist eine kurze beschreibung von dem Team</p>
+              </div>
+          </div>
+          <div class="col h-100 px-0 bg-secondary img-cover justify-content-center" style="background-image: url(https://images.unsplash.com/photo-1521146764736-56c929d59c83?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80);">
+              <div class="align-self-center w-75">
+                <h1 class="display-1">
+                  Hans Pater Team
+                </h1>
+                <p>Dies hier ist eine kurze beschreibung von dem Team und seinen fähigkeiten. Was sie alles so machen und tun und machen unfd tun und machen und tun und machen. Dies hier ist eine kurze beschreibung von dem Team</p>
+              </div>
+          </div>
         </div>
-        <div class="col h-100 px-0 bg-secondary img-cover justify-content-center" style="background-image: url(https://images.unsplash.com/photo-1606274741559-d322810275c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80);">
-            <div class="align-self-center w-75">
-              <h1 class="display-1">
-                Hans Pater Team
-              </h1>
-              <p>Dies hier ist eine kurze beschreibung von dem Team und seinen fähigkeiten. Was sie alles so machen und tun und machen unfd tun und machen und tun und machen. Dies hier ist eine kurze beschreibung von dem Team</p>
-            </div>
+        <div class="row h-10 w-100 px-0 mx-0">
+          <button type="button" name="button" class="btn btn-primary btn-lg btn-block px-0 mx-0">Abstimmen</button>
         </div>
       </div>
-      <div class="row h-10 w-100 px-0 mx-0">
-        <button type="button" name="button" class="btn btn-primary btn-lg btn-block px-0 mx-0">Abstimmen</button>
-      </div>
+    </div>
+    <div class="d-flex eao-vote-footer">
+      <span>Schulsprecherwahl STS Walddórfer</span>
     </div>
   </body>
 </html>

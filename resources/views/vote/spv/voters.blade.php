93f3007bef820b8fc46a059a810f20381b6d3cf2
j0ha <46874251+j0ha@users.noreply.github.com>
2020-12-20 12:09:46 +0100

    added voters voters view

---
+++ resources/views/vote/spv/voters.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title></title>
+  </head>
+  <body class="eao-vote-body">
+    <div class="d-flex eao-vote-header">
+      <span class="display-4">Bitte wähle deinen Namen!</span>
+    </div>
+    <div class="d-flex flex-column eao-vote-btn-list">
+      <button type="button" class="btn btn-primary eao-vote-btn-list-item">Primary</button>
+      <button type="button" class="btn btn-primary eao-vote-btn-list-item">Primary</button>
+      <button type="button" class="btn btn-primary eao-vote-btn-list-item">Primary</button>
+      <button type="button" class="btn btn-primary eao-vote-btn-list-item">Primary</button>
+      <button type="button" class="btn btn-primary eao-vote-btn-list-item">Primary</button>
+      <button type="button" class="btn btn-primary eao-vote-btn-list-item">Primary</button>
+      <button type="button" class="btn btn-primary eao-vote-btn-list-item">Primary</button>
+      <button type="button" class="btn btn-primary eao-vote-btn-list-item">Primary</button>
+      <button type="button" class="btn btn-primary eao-vote-btn-list-item">Primary</button>
+      <button type="button" class="btn btn-primary eao-vote-btn-list-item">Primary</button>
+      <button type="button" class="btn btn-primary eao-vote-btn-list-item">Primary</button>
+      <button type="button" class="btn btn-primary eao-vote-btn-list-item">Primary</button>
+      <button type="button" class="btn btn-primary eao-vote-btn-list-item">Primary</button>
+    </div>
+    <div class="d-flex eao-vote-footer">
+      <span>Schulsprecherwahl STS Walddórfer</span>
+    </div>
+  </body>
+</html>

2d9222f880836f564cdb5447288b5cee3ec72081
j0ha <46874251+j0ha@users.noreply.github.com>
2020-12-20 14:51:24 +0100

    added end vote view

---
+++ resources/views/vote/end.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title></title>
+  </head>
+  <body>
+
+    <body class="eao-vote-body">
+
+
+
+    <div class="d-flex align-items-center justify-content-center h-100">
+      <div class="w-50 h-30 d-flex justify-content-center align-items-center">
+        <div class="col">
+          <div class="row mb-5">
+            <img class="mx-auto" src="https://www.stadtteilschule-walddoerfer.de/fileadmin/stswa/images/theme/logo-stadtteilschule-walddoerfer.png" alt="">
+          </div>
+          <div class="row justify-content-center">
+            <span class="display-3">Schulspracherwahl STSWA 2020</span>
+          </div>
+           <div class="row justify-content-center">
+             <span>Vielen Dank für deine Stimme!</span>
+           </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>

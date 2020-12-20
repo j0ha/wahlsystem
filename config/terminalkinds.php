4d3e420f2ad98c1578bacbf9f09482c24f7e5a17
j0ha <46874251+j0ha@users.noreply.github.com>
2020-12-20 15:02:44 +0100

    added config for terminals-kinds

---
+++ config/terminalkinds.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+  'email' => [
+    'short' => 'email',
+    'name' => 'E-Mail',
+    'deletable' => false
+    ],
+    'normal' => [
+      'short' => 'normal',
+      'name' => 'Normal',
+      'deletable' => true
+      ],
+
+  ] ?>

b239fdb8bec5a8e86d39762fd01c69e6c4d44c3f
j0ha <46874251+j0ha@users.noreply.github.com>
2020-12-29 09:54:08 +0100

    Update 2020_08_10_190211_create_terminals_table.php
    fixed hits bug

--- database/migrations/2020_08_10_190211_create_terminals_table.php
+++ database/migrations/2020_08_10_190211_create_terminals_table.php
@@ -25,7 +25,7 @@ class CreateTerminalsTable extends Migration
             $table->ipAddress('ip_restriction')->nullable();
             $table->uuid('uuid');
             $table->integer('election_id');
-            $table->integer('hits');
+            $table->integer('hits')->default(0);
             $table->timestamps();
         });
     }

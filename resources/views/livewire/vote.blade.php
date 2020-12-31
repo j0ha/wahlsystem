7051668292c096e4cfa9aa112a95f99ff4cf9c5b
j0ha <46874251+j0ha@users.noreply.github.com>
2020-12-31 19:56:35 +0100

    changed vote bg to brighter

--- resources/views/livewire/vote.blade.php
+++ resources/views/livewire/vote.blade.php
@@ -156,7 +156,7 @@
         @foreach($spv_candidates as $candidate)
         <div class="col h-100 px-0">
 
-          <div class="h-90 bg-secondary img-cover justify-content-center" style="background-image: url({{$candidate->image}})">
+          <div class="h-90 eao-vote-bg-lite img-cover justify-content-center" style="background-image: url({{$candidate->image}})">
               <div class="align-self-center w-75">
                 <h1 class="display-1">
                   {{$candidate->name}}

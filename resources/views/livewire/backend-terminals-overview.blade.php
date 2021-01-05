b79aebc3a78d01eceeddf5da946bfd33bc9c9b9b
j0ha <46874251+j0ha@users.noreply.github.com>
2021-01-05 18:49:12 +0100

    fixed missed wire:ignore.self

--- resources/views/livewire/backend-terminals-overview.blade.php
+++ resources/views/livewire/backend-terminals-overview.blade.php
@@ -85,7 +85,7 @@
           </div>
         </div>
 
-        <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editeModalLabel" aria-hidden="true">
+        <div wire:ignore.self class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editeModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
